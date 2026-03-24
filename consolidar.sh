58f8fed323067513844d7e94290880c11f3435de
lucas-sosa-git <lucas.sosaet32@gmail.com>
2026-03-24 19:34:06 -0300

    Agregando el script consolidar.sh

---
+++ consolidar.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+while true; do
+	for archivo in "$HOME/EPNro1/entrada"/*.txt; do
+		if[[ -e "$archivo"]];; then
+			cat "$archivo" >> "$HOME/EPNRo1/salida/$FILENAME.txt"
+			mv "$archivo" "$HOME/EPNro1/procesado/"
+		fi
+	done
+done
+
