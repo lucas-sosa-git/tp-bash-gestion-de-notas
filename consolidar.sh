407c9c0276f200dc2112e19a2b1dfcde470aac61
Lukitas <75153216+lucas-sosa-git@users.noreply.github.com>
2026-03-24 20:43:58 -0300

    error de sintaxis por mayuscula

--- consolidar.sh
+++ consolidar.sh
@@ -3,7 +3,7 @@
 while true; do
 	for archivo in "$HOME/EPNro1/entrada"/*.txt; do
 		if [[ -e "$archivo" ]]; then
-			cat "$archivo" >> "$HOME/EPNRo1/salida/$FILENAME.txt"
+			cat "$archivo" >> "$HOME/EPNro1/salida/$FILENAME.txt"
 			mv "$archivo" "$HOME/EPNro1/procesado/"
 		fi
 	done
